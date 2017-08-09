c3a2b6c82870e79ea7a247274f3ffe1a27de88ab
Jae Kim <jaehoonkim1995@gmail.com>
2017-08-09 02:27:10 -0400

    plt_inline Example4: working varargs jump

---
+++ plt_inline/example04_varargs_test/inlineasm.c
@@ -0,0 +1,51 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdint.h>
+
+// Mimic wrapper function
+int aeu(int count,...)
+{
+  va_list ap;
+  int i, sum;
+
+  va_start (ap, count);         /* Initialize the argument list. */
+
+  sum = 0;
+  for (i = 0; i < count; i++)
+    sum += va_arg (ap, int);    /* Get the next argument value. */
+
+  va_end (ap);                  /* Clean up. */
+  return sum;
+}
+
+// Mimic dmtcp_sdlysm
+void * get_aeu()
+{
+  return (void *) &aeu;
+}
+
+// Mimic <symbol>__dmtcp_plt
+void * plt_aeu()
+{
+  /* RDI, RSI, RDX, RCX, R8, R9, XMM0–7 */
+  void * __fptr = get_aeu(); 
+  asm ("mov %%rbp, %%r11\n\t" 
+       "sub %%rsp, %%r11\n\t"
+       "add %%r11, %%rsp\n\t"
+       "pop %%rbp\n\t"
+       "jmp *%0"
+       : 
+       : "a" (__fptr)
+       : );
+  return NULL;
+}
+
+int main()
+{
+  /* Registers used: %edi %esi %edx  */
+  printf("Plt : Sum 10 + 20 = %ld (should be 30)\n", (int64_t)(plt_aeu(2, 10, 20)));
+
+  /* Registers used: %edi %esi %edx %ecx %r8d %r9d (rest pushed onto stack)  */
+  printf("Plt : Sum 1 to 10 = %ld (should be 55)\n", (int64_t)(plt_aeu(10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)));
+  return 0;
+}
