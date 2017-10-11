280c7d8aba69aa16279e480a169495471c182eb7
Jae Kim <jaehoonkim1995@gmail.com>
2017-10-10 18:53:31 -0700

    Working version with dmtcp

--- example_libdmtcp5/utilities/get_dmtcp_wrapper_symbols.sh
+++ example_libdmtcp5/utilities/get_dmtcp_wrapper_symbols.sh
@@ -8,7 +8,7 @@ grep -sh MACRO\( ${TMP_DMTCP_DIR_TMP}/src/* |
   sed 's/MACRO(\(.*\)).*/\1/' | 
   #grep -v -E '^(dlopen|dlclose|__libc_memalign|sigvec|__sigpause|__clone)$' | #THIS IS TEMPORARY *** *** ***`
   grep -v -E '^(dlopen|dlclose|__libc_memalign|sigvec|__sigpause)$' | #THIS IS TEMPORARY *** *** ***`
-  sort > dmtcp.wrapper.sym
+  sort | uniq > dmtcp.wrapper.sym
 
 # libdmtcppid
 #grep -sh MACRO\( ${TMP_DMTCP_DIR_TMP}/src/plugin/pid/* | egrep -o 'MACRO\([A-Za-z0-9_]+\)' | sed 's/MACRO(\(.*\)).*/\1/' | sort > dmtcp.pid.plugin.wrapper.sym
